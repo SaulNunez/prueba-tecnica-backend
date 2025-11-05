a6802e43e752b18a36e420279ca69caab68a7240
Saul Nunez <saul.nunez99@gmail.com>
2025-11-05 15:09:27 -0700

    Agregar DTO para evitar overposting

---
+++ Models/DTOs/ViajeDto.cs
@@ -0,0 +1,9 @@
+namespace prueba_tecnica_backend.Models.DTOs;
+
+public record ViajeDto(
+    string Origen,
+    string Destino,
+    DateTime FechaSalida,
+    DateTime FechaLlegada,
+    string Operador
+    );
