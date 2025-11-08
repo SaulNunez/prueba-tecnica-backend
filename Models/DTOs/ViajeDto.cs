2beac385bb9ba95f88d9117808e0163915a2b3e1
Saul Nunez <saul.nunez99@gmail.com>
2025-11-08 14:19:02 -0700

    Cambiar nombre de campos de fecha para hacer empate a los requisitos

--- Models/DTOs/ViajeDto.cs
+++ Models/DTOs/ViajeDto.cs
@@ -3,8 +3,8 @@ namespace prueba_tecnica_backend.Models.DTOs;
 public record ViajeDto(
     string Origen,
     string Destino,
-    DateTime FechaSalida,
-    DateTime FechaLlegada,
+    DateTime FechaInicio,
+    DateTime FechaFin,
     string Operador,
     int Id,
     int IdRuta,
